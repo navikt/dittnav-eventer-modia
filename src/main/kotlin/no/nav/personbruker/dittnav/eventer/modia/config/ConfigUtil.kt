bf246275f3ba6d1005105749a4d9d510db5640e4
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-08-25 14:17:03 +0200

    cleanup: sletting av ubrukt kode

--- src/main/kotlin/no/nav/personbruker/dittnav/eventer/modia/config/ConfigUtil.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.personbruker.dittnav.eventer.modia.config
-
-object ConfigUtil {
-
-    fun isCurrentlyRunningOnNais(): Boolean {
-        return System.getenv("NAIS_APP_NAME") != null
-    }
-}

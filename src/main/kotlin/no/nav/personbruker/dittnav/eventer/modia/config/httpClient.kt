c5d243ea712eb62a876ed2af3b4329895b4898b6
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-09-07 14:27:22 +0200

    list transformation workaround

--- src/main/kotlin/no/nav/personbruker/dittnav/eventer/modia/config/httpClient.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventer/modia/config/httpClient.kt
@@ -1,14 +1,16 @@
 package no.nav.personbruker.dittnav.eventer.modia.config // ktlint-disable filename
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.plugins.timeout
 import io.ktor.client.request.header
 import io.ktor.client.request.request
 import io.ktor.client.request.url
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpMethod
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import no.nav.personbruker.dittnav.eventer.modia.common.AzureToken
 import no.nav.tms.token.support.azure.exchange.service.AzureHeader
 import java.net.URL
@@ -24,5 +26,7 @@ suspend inline fun <reified T> HttpClient.getWithAzureAndFnr(url: URL, accessTok
             connectTimeoutMillis = 10000
             requestTimeoutMillis = 40000
         }
-    }.body()
+    }.let {
+        Json.decodeFromString(it.bodyAsText())
+    }
 }
